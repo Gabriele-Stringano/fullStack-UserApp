7b0bf0a0132a22190f62d6a65dedeca04182fb78
Gabriele-Stringano <gabrielestringano@gmail.com>
2023-03-17 15:18:47 +0100

    Added react-router with 3 new routes
    New dependenci : react-router-dom
    
    New routes
    path='/'
    path='/login'
    path='/signup'

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 
+//added react router for more pages
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+//import other components e styleDocumets
+import { SignupPage } from './components/signup/SignupPage';
+import { LoginPage } from './components/login/LoginPage';
+
 function App() {
   return (
-    <div className="App">
-      provone
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes className={'page'}>
+          <Route exact path='/' element={
+            <div>HOME</div>
+          } />
+
+          <Route exact path='/login' element={
+            <LoginPage/>
+          } />
+          <Route exact path='/signup' element={
+            <SignupPage/>
+          } />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/components/signup/SignupPage.js
@@ -0,0 +1,6 @@
+
+export function SignupPage(){
+    return(
+        <h1>Signup Page</h1>
+    )
+}
