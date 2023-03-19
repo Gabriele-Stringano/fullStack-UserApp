a78dd924463c87fff66d7203d1628f0f4d45a76a
Gabriele-Stringano <gabrielestringano@gmail.com>
2023-03-19 17:12:20 +0100

    Added Hashing Passwords
    - New dependency : bcrypt
    - Now passwords are hashed

--- server/models/users.js
+++ server/models/users.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const { isEmail } = require("validator");
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -16,7 +17,18 @@ const userSchema = new Schema({
         required: [true, 'Please enter a password'],
         minlength: [6, 'Minimum password length is 6 characters'],
     },
+});
+
+//Mongoose Hoonks post('save', function(doc, next)) -> fire a function after user saved to DB
+//Mongoose Hoonks pre -> fire a function before user saved to DB
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    //this refers to the instance of the user we're trying to create
+    //!!!instance is one particular copy of a thing!!!
+    this.password = await bcrypt.hash(this.password, salt);
+    //go to the next middleware, "If you don't use it, the code will crash."
+    next();
 })
 
 // the first parameter of the model must be the singular of the collection name
-module.exports = mongoose.model("user", userSchema)
+module.exports = mongoose.model("user", userSchema);
